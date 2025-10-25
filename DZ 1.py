8ec6383fc848f2dfdbb47085c0d6ae5d0d80c57e
vladislav96qwerty <vladrudoy320@gmail.com>
2025-10-25 20:13:50 +0300

    DZ 1

---
+++ DZ 1.py
@@ -0,0 +1,10 @@
+print("Hello Teacher")
+number = int(input("Введіть 4-значне число: "))
+a1 = number // 1000
+a2 = (number // 100) % 10
+a3 = (number // 10) % 10
+a4 = number % 10
+print(a1)
+print(a2)
+print(a3)
+print(a4)
