83fda5f5194bb078d36d8c5dc0670b5c30eb879c
vladislav96qwerty <vladrudoy320@gmail.com>
2025-10-25 19:10:23 +0300

    DZ

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
-print('Hello World')
-age=29
-print(age)
+from math import remainder
+
+print('Hello Teacher')
+number = int(input("Введіть 4-х значне число: "))
+a1 = number // 1000
+a2 = (number // 100) % 10
+a3 = (number // 10) % 10
+a4 = number % 10
+print(a1)
+print(a2)
+print(a3)
+print(a4)
+
+
+
