d1b161ff8ecda5ec3a2a7ace79a5ef72da62c5e0
vladislav96qwerty <vladrudoy320@gmail.com>
2025-10-26 18:34:39 +0200

    DZ 1

---
+++ Тренировка 1.py
@@ -0,0 +1,9 @@
+user_name = input("Как вас завут?")
+print("Добрый вечер," +user_name+ "!Рад с вами познакомиться")
+birth_year=int(input("В каком году вы родились?"))
+current_year=2025
+age=current_year-birth_year
+future_age=age+10
+print("Сейчас вам", age, "лет")
+print("Через 10 лет вам будет", future_age, "лет")
+
