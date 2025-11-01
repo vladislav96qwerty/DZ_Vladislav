de1d3664e2f4ff3ccdddd1fc3e0f5a0ddc4f118f
vladislav96qwerty <vladrudoy320@gmail.com>
2025-11-01 12:05:44 +0200

    DZ 6

--- DZ 6.py
+++ DZ 6.py
@@ -1,6 +1,15 @@
-def zeros(lst):
-    zero_count = lst.count(0)
-    while 0 in lst:
-        lst.remove(0)
+lists = [
+    [0, 8, 0, 24, 5],
+    [0],
+    [9, 0, 66, 0, 0, 0, 5],
+    [4, 0, 6, 31, 0, 55, 0, 47, 0, 45, 0, 0, 86, 0]
+]
+for first_list in lists:
+    new_list = []
+    for n in first_list:
+        if n != 0:
+            new_list.append(n)
+    zero_count = first_list.count(0)
     for i in range(zero_count):
-        lst.append(0)
+        new_list.append(0)
+    print(new_list)
---
+++ Тренировка 1.py
@@ -0,0 +1,3 @@
+first_list = [3, 4, 0, 4, 5, 34,0, 5, 35,0,3, 14,0, 25,0]
+first_list.sort(reverse=True) # сортування від більшого до меншого
+print(first_list)
