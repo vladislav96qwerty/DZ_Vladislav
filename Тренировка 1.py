bf85227327bf655b5b636c49789d1287f4951afc
vladislav96qwerty <vladrudoy320@gmail.com>
2025-11-01 12:11:06 +0200

    DZ 6

--- Тренировка 1.py
+++ Тренировка 1.py
@@ -1,3 +1,6 @@
-first_list = [3, 4, 0, 4, 5, 34,0, 5, 35,0,3, 14,0, 25,0]
-first_list.sort(reverse=True) # сортування від більшого до меншого
-print(first_list)
+def zeros(lst):
+    zero_count = lst.count(0)
+    while 0 in lst:
+        lst.remove(0)
+    for i in range(zero_count):
+        lst.append(0)
